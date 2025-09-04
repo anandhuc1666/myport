607481f2de50065ee0c4b1e1bad8a189596a154b
anandhuc1666 <anandhuc166@gmail.com>
2025-09-04 16:34:19 +0530

    Portfolio

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,6 +3,9 @@ import './Body.css'
 import boy from '../assets/Boylogo.png'
 import arrow from '../assets/arrow.png'
 import Typewriter from '../components/Typewriter'
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 
 function Body() {
     return (
@@ -25,6 +28,11 @@ function Body() {
             </div>
             <div className="boy-imgs">
                 <img src={boy} alt="" className='boy-logos' />
+            </div>
+            <div className="socialMd">
+                <a href="https://www.linkedin.com/in/anandhu-c-267956253"><FaLinkedin className='s-Media' /></a>
+                <a href=""><FaInstagramSquare className='s-Media' /></a>
+                <a href="https://github.com/anandhuc1666"><FaGithubSquare className='s-Media' /></a>  
             </div>
 
         </div>
