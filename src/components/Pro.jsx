67ad238874850f92164556a409d40d21e81edb35
anandhuc1666 <anandhuc166@gmail.com>
2025-09-04 13:21:18 +0530

    portfolio

--- src/components/Pro.jsx
+++ src/components/Pro.jsx
@@ -7,6 +7,7 @@ import { TbBrandJavascript } from "react-icons/tb";
 import { SiTailwindcss } from "react-icons/si";
 import { SiRedux } from "react-icons/si";
 import { SiCanva } from "react-icons/si";
+import Typewriter from './Typewriter';
 import logo from '../assets/ADlogo.png'
 import pic1 from '../assets/Screenshot 2025-07-02 200332.png'
 import pic2 from '../assets/Screenshot 2025-07-02 200511.png'
@@ -22,29 +23,36 @@ function Pro() {
                 <div className="pro-box">
                     <div className="project-box">
                         <img src={pic1} alt="" className='pic' />
-                        <p>Bags can be used to carry items such as personal belongings, groceries, tools, and other objects. They come in various shapes and sizes, often equipped with handles or straps for easier carrying.</p>
+                        <p>Designed and built a feature-rich e-commerce website specializing in high-quality bags. The platform provides
+                            a seamless shopping experience for customers seeking both curated selections and the ability to personalize
+                            their bags with custom designs.</p>
                     </div>
                     <div className="project-box">
                         <img src={pic2} alt="" className='pic' />
-                        <p>Bags can be used to carry items such as personal belongings, groceries, tools, and other objects. They come in various shapes and sizes, often equipped with handles or straps for easier carrying.</p>
+                        <p>Discover a wide range of high-quality plants, carefully selected to brighten your space. Explore, compare,
+                            and purchase the perfect plant — all in one place. Shop with confidence and bring nature home!</p>
                     </div>
                     <div className="project-box">
                         <img src={pic3} alt="" className='pic' />
-                        <p>Bags can be used to carry items such as personal belongings, groceries, tools, and other objects. They come in various shapes and sizes, often equipped with handles or straps for easier carrying.</p>
+                        <p>Struggling with studies? We’ve got you covered! Watch clear, subject-wise study videos wherever you are, and
+                            if you get stuck, simply connect with personal teachers for quick help. Learning has never been this easy!</p>
                     </div>
                     <div className="project-box">
                         <img src={pic4} alt="" className='pic' />
-                        <p>Bags can be used to carry items such as personal belongings, groceries, tools, and other objects. They come in various shapes and sizes, often equipped with handles or straps for easier carrying.</p>
+                        <p>Celebrate love with ease! Track your wedding countdown, download your wedding photos, and book your special
+                            date — all in one simple platform.</p>
                     </div>
                     <div className="project-box">
                         <img src={pic5} alt="" className='pic' />
-                        <p>Bags can be used to carry items such as personal belongings, groceries, tools, and other objects. They come in various shapes and sizes, often equipped with handles or straps for easier carrying.</p>
+                        <p>A front-end clone of the Goibibo website, built to demonstrate my skills in responsive web design and user
+                            interface development using HTML, CSS.</p>
                     </div>
                 </div>
             </div>
             <div className="end-box">
                 <div className="end-box1">
-                    <h2>I'm currently looking to join a <span>cross-function</span> team </h2>
+                    
+                    <h2>I'm currently looking to join a <span><Typewriter text='cross-function' speed={100}/></span> team </h2>
                     <p>that values imroving perople's lives through accessible design</p>
                     <div className="end-box11">
                         <FaReact className='lag' />
@@ -67,13 +75,13 @@ function Pro() {
                     <div className="cir7"></div>
                     <div className="cir8">
                         <img src={logo} alt="" style={{ width: 200 }} className='cir8-text' />
-                        <FaReact className='lag1r' style={{marginTop:"-150px",marginLeft:"250px"}}/>
-                        <FaHtml5 className='lag1h' style={{marginTop:"150px",marginLeft:"-200px"}}/>
-                        <FaCss3 className='lag1c' style={{marginLeft:"-350px"}}/>
-                        <SiTailwindcss className='lag1t' style={{marginTop:"-250px",marginLeft:"-100px"}}/>
-                        <TbBrandJavascript className='lag1j' style={{marginTop:"20px",marginLeft:"200px"}}/>
-                        <SiRedux className='lag1x' style={{marginTop:"-330px",marginLeft:"100px"}}/>
-                        <SiCanva className='lag1ca'style={{marginTop:"300px",marginLeft:"150px"}} />
+                        <FaReact className='lag1r' style={{ marginTop: "-150px", marginLeft: "250px" }} />
+                        <FaHtml5 className='lag1h' style={{ marginTop: "150px", marginLeft: "-200px" }} />
+                        <FaCss3 className='lag1c' style={{ marginLeft: "-350px" }} />
+                        <SiTailwindcss className='lag1t' style={{ marginTop: "-250px", marginLeft: "-100px" }} />
+                        <TbBrandJavascript className='lag1j' style={{ marginTop: "20px", marginLeft: "200px" }} />
+                        <SiRedux className='lag1x' style={{ marginTop: "-330px", marginLeft: "100px" }} />
+                        <SiCanva className='lag1ca' style={{ marginTop: "300px", marginLeft: "150px" }} />
 
                     </div>
 
